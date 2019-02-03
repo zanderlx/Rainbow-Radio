9833f26d588e529c270f71c9d0f6af48131d8037
zanderlx <lsaplan97@gmail.com>
2019-02-03 00:45:14 -0800

    Added "Double Click On Song To Play" feature

--- src/csulb/cecs327/AppUI.java
+++ src/csulb/cecs327/AppUI.java
@@ -4,8 +4,11 @@
 
 package csulb.cecs327;
 
+import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
 import com.jgoodies.forms.factories.*;
 import net.miginfocom.swing.*;
 
@@ -29,6 +32,7 @@ public class AppUI extends JPanel implements ActionListener {
     private JSlider volumeSlider;
     private int currentSong = 0;
     private SongDatabase songDatabase;
+    DefaultTableModel model;
 
     // JFormDesigner - End of variables declaration  //GEN-END:variables
     String[][] data = {
@@ -40,7 +44,7 @@ public class AppUI extends JPanel implements ActionListener {
     String[] columnNames = {"Name", "Roll Number", "Department"};
 
     // Music Player Variables
-    MusicPlayer player;
+    private MusicPlayer player;
 
     // Constructor
     public AppUI() {
@@ -73,9 +77,14 @@ public class AppUI extends JPanel implements ActionListener {
             new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(0, 0, 0, 0),
                 "JFormDesigner Evaluation", javax.swing.border.TitledBorder.CENTER,
                 javax.swing.border.TitledBorder.BOTTOM, new java.awt.Font("Dialog", java.awt.Font.BOLD, 12),
-                java.awt.Color.red), getBorder())); addPropertyChangeListener(new java.beans.PropertyChangeListener(){public void propertyChange(java.beans.PropertyChangeEvent e){if("border".equals(e.getPropertyName()))throw new RuntimeException();}});
+                java.awt.Color.red), getBorder()));
 
-        setLayout(new MigLayout(
+        addPropertyChangeListener(e -> {
+            if ("border".equals(e.getPropertyName()))
+                throw new RuntimeException();
+        });
+
+        setLayout( new MigLayout(
             "hidemode 3",
             // columns
             "[fill]" +
@@ -154,6 +163,18 @@ public class AppUI extends JPanel implements ActionListener {
         //======== songInfoPane ========
         {
             songInfoPane.setViewportView(songInfoTable);
+
+            Object[] columns = { "No.", "Song Title", "Artist", "Album" };
+            model = new DefaultTableModel() {
+                @Override
+                public boolean isCellEditable(int row, int column) {
+                    return false;
+                }
+            };
+            model.setColumnIdentifiers(columns);
+            songInfoTable.getTableHeader().setReorderingAllowed(false);
+            addDefaultTableRows();
+            songInfoTable.setModel(model);
         }
         add(songInfoPane, "cell 14 2 21 22");
 
@@ -161,8 +182,8 @@ public class AppUI extends JPanel implements ActionListener {
         {
             playlistPane.setViewportView(playlistItems);
         }
-        add(playlistPane, "cell 0 3 8 24,growy");
-        add(volumeLabel, "cell 32 27,alignx center,growx 0");
+        add(playlistPane, "cell 0 3 8 24, growy");
+        add(volumeLabel, "cell 32 27, alignx center, growx 0");
 
         //---- playPauseButton ----
         playPauseButton.setText("Play");
@@ -184,6 +205,8 @@ public class AppUI extends JPanel implements ActionListener {
         add(muteButton, "cell 28 28");
         add(volumeSlider, "cell 29 28 7 1,align center top,grow 0 0");
         // JFormDesigner - End of component initialization  //GEN-END:initComponents
+
+        playSongOnDoubleClick();
     }
 
     // Initializing the JTable
@@ -191,13 +214,40 @@ public class AppUI extends JPanel implements ActionListener {
         this.songInfoTable = songInfoTable;
     }
 
+    public void addDefaultTableRows() {
+        model.addRow(new Object[] {"1", "Faded", "Alan Walker", "Different World" });
+        model.addRow(new Object[] {"2", "Mine", "Bazzi", "Cosmic" });
+        model.addRow(new Object[] {"3", "Crab Rave", "Noisestorm", "Monstercat" });
+        model.addRow(new Object[] {"4", "High Hopes", "Panic! At The Disco", "Pray for the Wicked" });
+        model.addRow(new Object[] {"5", "Stressed Out", "Twenty One Pilots", "Blurryface" });
+    }
+
+    private void playSongOnDoubleClick() {
+        songInfoTable.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 2) {
+                    //System.out.println(songInfoTable.getValueAt(songInfoTable.getSelectedRow(), 1) + " - " + songInfoTable.getValueAt(songInfoTable.getSelectedRow(), 0) + " (8D AUDIO).mp3");
+                    currentSong = songInfoTable.getSelectedRow();
+                    System.out.println(currentSong);
+                    player.stop();
+                    String song = songDatabase.getSongList().get(currentSong);
+                    player = new MusicPlayer(song);
+                    playPauseButton.setText("Pause");
+                    player.play();
+                }
+            }
+        });
+    }
+
+
+
     /**
      * This method will allow the functionality of playing or pausing a song
      *
      *
      */
     private void playPauseButtonActionPerformed() {
-        // TODO add your code here
         System.out.println(currentSong);
         String song = songDatabase.getSongList().get(currentSong);
         player = new MusicPlayer(song);
