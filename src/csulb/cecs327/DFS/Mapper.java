aade0f514486bf6f682903e02c21c64f5fd7ff4c
FryedBryce <brycemoser@yahoo.com>
2019-04-29 03:07:07 -0700

    Mapper WIP

--- src/csulb/cecs327/DFS/Mapper.java
+++ src/csulb/cecs327/DFS/Mapper.java
@@ -1,6 +1,7 @@
 package csulb.cecs327.DFS;
 
 import com.google.gson.JsonObject;
+import java.util.*;
 
 import java.io.IOException;
 import java.io.Serializable;
@@ -10,6 +11,10 @@ import java.security.NoSuchAlgorithmException;
 
 public class Mapper implements MapReduceInterface, Serializable {
     //TODO These 2 methods
+
+    TreeMap<String,JsonObject> map = new TreeMap<String, JsonObject>();
+
+
 
     /**
      * Distribute the words throughout the file system, putting the word according to its guid between 2 processes
@@ -21,6 +26,17 @@ public class Mapper implements MapReduceInterface, Serializable {
      */
     public void map(String key, JsonObject value, DFS context, String file) throws IOException {
         //TODO
+        String[] letters = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "-", "+"};
+
+        String newKey = value.get(key).getAsString();
+        String newValue="";
+        String first = String.valueOf(newKey.charAt(0));
+        for(int i=0; i<=letters.length;i++){
+            if(letters[i] == first){
+                newValue = Integer.toString(i);
+            }
+        }
+        context.emit(newKey, newValue, file);
     }
 
     public void reduce(String key, JsonObject values, DFS context, String file) throws IOException {
